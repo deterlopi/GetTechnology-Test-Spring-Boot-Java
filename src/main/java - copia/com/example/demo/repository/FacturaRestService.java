079873226416f89373bde11b73bc068af0b8ae06
deterlopi <deterlopi@gmail.com>
2022-07-18 04:29:36 -0600

    //new

--- src/main/java - copia/com/example/demo/repository/FacturaRestService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.FacturaRepository;
-
-public interface FacturaRestService extends JpaRepository<FacturaRepository, Long> {
-
-}
--- src/main/java/com/example/demo/controllers/VentasController.java
+++ src/main/java/com/example/demo/controllers/VentasController.java
@@ -31,4 +31,5 @@ public class VentasController {
         return this.FacturaService.guardarFactura(factura);
     }
 
+
 }
